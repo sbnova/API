b90d7a370d8068bad11007210e47cabd23dcf352
test <test1>
2019-01-13 10:41:46 +0900

    메인 액티비티 초안 구성

--- app/src/main/java/com/cyj/api/MainActivity.java
+++ app/src/main/java/com/cyj/api/MainActivity.java
@@ -3,11 +3,30 @@ package com.cyj.api;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
